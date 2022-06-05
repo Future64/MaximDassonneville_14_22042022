a8ce9afd815ccbff038d09e244df7858de43197b
Future64 <developement.future@gmail.com>
2022-06-05 18:45:39 +0200

    error404

--- src/pages/error404/Error404.js
+++ src/pages/error404/Error404.js
@@ -1,12 +1,15 @@
 import React from 'react'
-import { useState, useEffect, useContext, useRef, useMemo, useCallback } from 'react'
-import { useParams, useLocation, useMatch} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import './Error404.css'
 
 const Error404 = () => {
+  const titleError = 'Erreur 404!'
   return (
     <>
-    
+      <div className="boxError">
+        <h2>{titleError}</h2>
+        <Link to="/">CURRENT EMPLOYEE</Link>
+      </div>
     </>
   )
 }
